b8531c0d1b7e65e6ba3e877ab1d72e6cba67e3e0
joepamedia <josephpalmisano18@gmail.com>
2022-02-12 16:10:01 -0600

    added splide and updated code section

---
+++ index.js
@@ -0,0 +1,27 @@
+const toggle = document.getElementById("toggle");
+const body = document.querySelector("body");
+toggle.onclick = function () {
+  toggle.classList.toggle("active");
+  body.classList.toggle("active");
+};
+
+//splide width
+var splide = new Splide(".splide", ".splide2", {
+  type: "loop",
+  height: "10rem",
+  focus: "center",
+  autoWidth: true,
+});
+
+splide.mount();
+
+// splide height
+var splide = new Splide(".splide", ".splide2", {
+  type: "loop",
+  direction: "ttb",
+  height: "20rem",
+  focus: "center",
+  autoHeight: true,
+});
+
+splide.mount();
--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("toggle");
-const body = document.querySelector("body");
-toggle.onclick = function () {
-  toggle.classList.toggle("active");
-  body.classList.toggle("active");
-};
