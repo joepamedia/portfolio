4c422029e915ecdd7cc8e7a17383413f325410e9
joepamedia <josephpalmisano18@gmail.com>
2022-04-24 16:55:21 -0500

    updated coding and navbar section

--- index.js
+++ index.js
@@ -25,3 +25,12 @@ var splide = new Splide(".splide", ".splide2", {
 });
 
 splide.mount();
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
